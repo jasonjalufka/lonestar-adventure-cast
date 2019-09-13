3b5e50dac29bf66fd72c3f777647b916efadcc23
Jason Jalufka <jasonjalufka@gmail.com>
2019-09-13 14:48:15 -0700

    Fix: Podcast Column Layout (#7)
    * Add podcast post icons
    
    * Alter embedded soundcloud player to take up 100% height
    
    * Add runtime field to podcast section
    
    * Update podcast page to display all episodes with player
    
    * Update podcast post with runtime
    
    * Fix column layout, add padding between podcasts

--- src/pages/podcast/index.js
+++ src/pages/podcast/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../../components/Layout'
-import PodcastRoll from '../../components/PodcastRoll'
 import Soundcloud from '../../components/Soundcloud'
 import calendar from '../../img/calendar-icon.svg'
 import clock from '../../img/clock-icon.svg'
@@ -32,53 +31,52 @@ const PodcastIndexPage = ({ data }) => {
       <section className='section'>
         <div className='container'>
           <div className='content'>
-            {/* <PodcastRoll /> */}
-            <div className='columns'>
               {podcasts &&
                 podcasts.map(({ node: post }) => (
-                  <div className='column is-10 is-offset-1 box' key={post.id}>
-                    <div className='columns'>
-                      <div className='column is-4'>
-                        <Soundcloud html={post.htmlAst} />
-                      </div>
-                      <div className='column'>
-                        <h3>{post.frontmatter.title}</h3>
-                        <p
-                          style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            color: '#9B9B9B',
-                            fontSize: '15px'
-                          }}
-                        >
-                          <img
-                            src={calendar}
-                            alt='calendar'
+                  <div className='columns' style={{ paddingBottom: '40px' }}>
+                    <div className='column is-10 is-offset-1 box' key={post.id}>
+                      <div className='columns'>
+                        <div className='column is-4'>
+                          <Soundcloud html={post.htmlAst} />
+                        </div>
+                        <div className='column'>
+                          <h3>{post.frontmatter.title}</h3>
+                          <p
                             style={{
-                              width: '15px',
-                              marginRight: '6px',
-                              marginLeft: '10px'
+                              display: 'flex',
+                              alignItems: 'center',
+                              color: '#9B9B9B',
+                              fontSize: '15px'
                             }}
-                          />
-                          {post.frontmatter.date}
-                          <img
-                            src={clock}
-                            alt='clock'
-                            style={{
-                              width: '15px',
-                              marginRight: '6px',
-                              marginLeft: '20px'
-                            }}
-                          />
-                          {post.frontmatter.runtime}
-                        </p>
-                        <p>{post.frontmatter.description}</p>
+                          >
+                            <img
+                              src={calendar}
+                              alt='calendar'
+                              style={{
+                                width: '15px',
+                                marginRight: '6px',
+                                marginLeft: '10px'
+                              }}
+                            />
+                            {post.frontmatter.date}
+                            <img
+                              src={clock}
+                              alt='clock'
+                              style={{
+                                width: '15px',
+                                marginRight: '6px',
+                                marginLeft: '20px'
+                              }}
+                            />
+                            {post.frontmatter.runtime}
+                          </p>
+                          <p>{post.frontmatter.description}</p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 ))}
             </div>
-          </div>
         </div>
       </section>
     </Layout>
