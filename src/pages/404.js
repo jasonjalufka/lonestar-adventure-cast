616d124bc479aa3c8c424a05197f514e6a1897bd
Jason Jalufka <jasonjalufka@gmail.com>
2019-09-16 21:32:54 -0700

    Update 404 Page (#12)

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,41 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <div
+        className='full-width-image-container margin-top-0'
+        style={{
+          backgroundImage: `url('/img/404.jpg')`,
+          backgroundPosition: `50% 80%`,
+          height: `93vh`,
+          marginBottom: `0`
+        }}
+      >
+        <div
+        className='image-overlay'
+        style={{
+          width: `100%`,
+          height: `100%`,
+          backgroundColor: `rgba(0, 0, 0, 0.4)`,
+          display: `flex`,
+          flexDirection: `column`,
+          justifyContent: `space-around`
+        }}
+      >
+        <h1
+          className='has-text-weight-bold is-size-3-mobile is-size-2-tablet'
+          style={{
+            color: 'white',
+            padding: '1rem',
+            fontWeight: '900',
+            fontSize: '4.5rem',
+            textAlign: 'center',
+            textShadow: `#0000004f 0px 0px 5px`
+          }}
+        >
+          404: Page Not Found
+        </h1>
+      </div>
+      </div>
   </Layout>
 )
 
