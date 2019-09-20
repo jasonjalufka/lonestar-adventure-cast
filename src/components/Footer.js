d66ed6de10d5fb3781bcae15cbd6633e25faf4ee
Jason Jalufka <jasonjalufka@gmail.com>
2019-09-20 13:27:00 -0700

    Add link to Contributors Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -71,6 +71,11 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
+                      <Link className='navbar-item' to='/contributors'>
+                        Contributors
+                      </Link>
+                    </li>
+                    <li>
                       <Link className='navbar-item' to='/contact'>
                         Contact
                       </Link>
