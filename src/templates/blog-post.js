3e3230e93f6e2dc6aadb4e52ab505cfa4dcd9faf
Jason Jalufka <jasonjalufka@gmail.com>
2019-09-23 15:11:31 -0700

    Add Author name, link, photo to blog post page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,32 +5,46 @@ import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const BlogPostTemplate = ({
+  authorImage,
+  authorLink,
+  authorName,
   content,
   contentComponent,
   description,
   tags,
   title,
-  helmet,
+  helmet
 }) => {
   const PostContent = contentComponent || Content
 
   return (
-    <section className="section">
+    <section className='section'>
       {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+      <div className='container content'>
+        <div className='columns'>
+          <div className='column is-10 is-offset-1'>
+            <h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
               {title}
             </h1>
+            <div
+              style={{ width: '75px', height: '75px', display: 'inline-block' }}
+            >
+              {authorName && (
+                <Link to={authorLink}>
+                  <PreviewCompatibleImage imageInfo={authorImage} />
+                  {authorName}
+                </Link>
+              )}
+            </div>
             <p>{description}</p>
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
                 <h4>Tags</h4>
-                <ul className="taglist">
+                <ul className='taglist'>
                   {tags.map(tag => (
                     <li key={tag + `tag`}>
                       <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
@@ -51,11 +65,12 @@ BlogPostTemplate.propTypes = {
   contentComponent: PropTypes.func,
   description: PropTypes.string,
   title: PropTypes.string,
-  helmet: PropTypes.object,
+  helmet: PropTypes.object
 }
 
 const BlogPost = ({ data }) => {
   const { markdownRemark: post } = data
+  const { author: authorData } = post.fields
 
   return (
     <Layout>
@@ -64,16 +79,19 @@ const BlogPost = ({ data }) => {
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
         helmet={
-          <Helmet titleTemplate="%s | Blog">
+          <Helmet titleTemplate='%s | Blog'>
             <title>{`${post.frontmatter.title}`}</title>
             <meta
-              name="description"
+              name='description'
               content={`${post.frontmatter.description}`}
             />
           </Helmet>
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
+        authorName={authorData.frontmatter.fullName || ''}
+        authorLink={authorData.fields.slug || ''}
+        authorImage={authorData.frontmatter.image || {}}
       />
     </Layout>
   )
@@ -81,8 +99,8 @@ const BlogPost = ({ data }) => {
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
+    markdownRemark: PropTypes.object
+  })
 }
 
 export default BlogPost
@@ -91,6 +109,23 @@ export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
+      fields {
+        author {
+          fields {
+            slug
+          }
+          frontmatter {
+            fullName
+            image {
+              childImageSharp {
+                fluid(maxWidth: 360, quality: 20) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
